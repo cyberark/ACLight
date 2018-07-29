aa059b83333f5b99b5f32a0a431044c41bc7300f
Asaf Hecht <Hechtov@users.noreply.github.com>
2018-07-29 14:44:44 +0300

    Delete Execute-ACLight2.bat

--- ACLight2/Execute-ACLight2.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-set var=%~d0%~p0%
-cd "%var%"
-set "var=%cd%\ACLight2.psm1"
-echo.
-echo  Welcome, starting Multi-Layered ACLight scan
-powershell -noprofile -ExecutionPolicy Bypass Import-Module '%var%' -force ; Start-ACLsAnalysis
-pause
